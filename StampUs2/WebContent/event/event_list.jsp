cb8d0d5d06505b208a59b1d804b12f2bd41e84a3
Suyeon <zosand123@naver.com>
2021-01-03 19:42:29 +0900

    update event_list, EventDAO

--- StampUs2/WebContent/event/event_list.jsp
+++ StampUs2/WebContent/event/event_list.jsp
@@ -11,41 +11,24 @@
 	session.setAttribute("user_id", "user1");
 %>
 <%
-    // DB 연결
-    Class.forName("oracle.jdbc.driver.OracleDriver");
-	String url = "jdbc:oracle:thin:@211.205.104.35:1521:xe";
-	String uid = "ky";
-	String upw = "1234";
-    Connection conn=DriverManager.getConnection(url,uid,upw); // db정보,아이디,비번
-   
-    // limit에 들어갈 index번호 생성
-    int index;  // limit에 전달할 index값
-    int pager;  // 현재 나타내고자 하는 페이지 값을 저장
-    if(request.getParameter("pager")==null){
-    	pager=1;
-    }else{
-    	pager=Integer.parseInt(request.getParameter("pager"));
-    }
-    // index 만들기
-    index=(pager-1)*10;
-    // 쿼리 생성
-     String sql = "select * from (select row_number() over (order by event_no desc) num, A.* from event A order by event_no desc) where num between " 
-    				+ index + " and " + (index + 10);
-    // 심부름꾼생성
-    Statement stmt=conn.createStatement();
-    // 쿼리 실행 => ResultSet
-    ResultSet rs=stmt.executeQuery(sql);
+	String pager=request.getParameter("pager");
+	if(pager==null){
+		pager="1";
+	}
+	EventDTO eDTO = new EventDTO();
+	EventDAO eDAO = new EventDAO();
+    ArrayList<EventDTO> list=eDAO.pageList(pager);
 %>    
 <jsp:include page="../header.jsp" />
-  <style>
-    a {
-      text-decoration:none;
-      color:black;
-    }
-    a:hover {
-      text-decoration:underline;
-    }
-  </style>
+<style>
+	a {
+	  text-decoration:none;
+	  color:black;
+	}
+	a:hover {
+	  text-decoration:underline;
+	}
+</style>
 <section id="event_list">
 	<div class="container">
 	<table class="table table-hover">
@@ -55,12 +38,12 @@
 		<td>작성일</td>
 		<td>조회수</td>
 	</tr>
-<%while(rs.next()){ %>
+<%for(int i=0; i<list.size(); i++){ %>
 	<tr>
-		<td><%=rs.getInt("event_no")%></td>
-		<td><a href="event_getView.jsp?event_no=<%=rs.getInt("event_no")%>"><%=rs.getString("event_title")%></a></td>
-		<td><%=rs.getString("event_postday")%></td>
-		<td><%=rs.getInt("event_view")%></td>
+		<td><%=list.get(i).getEvent_no()%></td>
+		<td><a href="event_getView.jsp?event_no=<%=list.get(i).getEvent_no()%>"><%=list.get(i).getEvent_title()%></a></td>
+		<td><%=list.get(i).getEvent_postday()%></td>
+		<td><%=list.get(i).getEvent_view()%></td>
 	</tr>
 <% }%>
 	<tr>
@@ -69,28 +52,11 @@
 	</tr>
 	<tr>
 		<td colspan="4" align="center">
-			   <%
-         // 총 페이지값을 구하기     총레코드수/페이지당레코드수
-         sql="select count(*) as cnt from event";
-         ResultSet rs2=stmt.executeQuery(sql);
-         rs2.next();
-         int page_cnt=rs2.getInt("cnt")/10+1;
-         
-         if(rs2.getInt("cnt")%10 == 0) 
-             page_cnt--;
-
-         int pstart;
-       
-         pstart=pager/10; 
-         if(pager%10 ==0)
-        	 pstart=pstart-1;
-         
-         pstart=Integer.parseInt(pstart+"1");
-         
-         int pend=pstart+9;  // 251+9 => 260  총페이지 :254
-         
-         if(page_cnt < pend)
-        	 pend=page_cnt;
+		<%
+         // 총 페이지값을 구하기  
+         	int page_cnt=eDAO.getTotalPage();
+       		int pstart=eDAO.getPstart(page_cnt,pager);
+		 	int pend=eDAO.getPend(page_cnt,pstart); 
 		%>
         <!-- 이전페이지 -->
         <!-- 현재 페이지 그룹 이전 10페이지 -->
@@ -107,9 +73,9 @@
        %>
         <!-- 현재페이지 기준 1페이지 이전 -->
        <%
-         if(pager != 1){
+         if(Integer.parseInt(pager) != 1){
        %>  
-        <a href="event_list.jsp?pager=<%=pager-1%>">◀  </a>
+        <a href="event_list.jsp?pager=<%=Integer.parseInt(pager)-1%>">◀  </a>
        <% 
          }else {
        %>
@@ -117,7 +83,7 @@
        <% 
          } for(int i=pstart;i<=pend;i++){
              String str="";
-        	 if(pager == i)
+        	 if(Integer.parseInt(pager) == i)
         		 str="style='color:red;'";
        %>
          <a href="event_list.jsp?pager=<%=i%>" <%=str%>> <%=i%> </a> <!-- 페이지네이션 -->
@@ -127,9 +93,9 @@
        <!-- 다음페이지 -->
        <!-- 현재페이지 기준 1페이지 이후 -->
        <%
-         if(pager != page_cnt){
+         if(Integer.parseInt(pager) != page_cnt){
        %>
-      <a href="event_list.jsp?pager=<%=pager+1%>"> ▶ </a>
+      <a href="event_list.jsp?pager=<%=Integer.parseInt(pager)+1%>"> ▶ </a>
       <%
          }else{
       %>
--- StampUs2/src/dao/EventDAO.java
+++ StampUs2/src/dao/EventDAO.java
@@ -3,18 +3,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import dto.EventDTO;
 import dto.Study_my_DTO;
 
-//event_view 기본값 0 으로 설정하기 
-//private String no;
-//private String adm;
-//private String title;
-//private String content;
-//private String img;
-//private String view;
 public class EventDAO {	
 	   String url = "jdbc:oracle:thin:@211.205.104.35:1521:xe";
 	   String uid = "ky";
@@ -88,35 +82,6 @@ public class EventDAO {
 		//자원해제
 		conn.close();
 	}
-	//=====================list.jsp===============================
-										//페이징처리를 위한 객체 전달받기
- 		public ArrayList<EventDTO> list() throws Exception{
-			//쿼리생성 - 정렬된 데이터 가져오기
-			String sql="select * from event order by event_no desc";
-			//2 정렬된 데이터에서 순서번호 붙이기
-			sql="select rownum rnum, event_no, event_adm, event_title, event_postday, event_view from ("+sql+")";
-			//3.원하는 부분에 해당되는 데이터 가져오기
-			sql="select event_no, event_adm, event_title, event_postday, event_view from ("+sql+")"
-					+" where rnum between ? and ? ";
-			//심부름꾼
-			PreparedStatement pstmt=conn.prepareStatement(sql);
-			//쿼리실행
-			ResultSet rs = pstmt.executeQuery();
-			
-			//보드디티오를 여러개 담을 수 있는 어레이리스트생성
-			ArrayList<EventDTO> list=new ArrayList<EventDTO>();
-			
-			while(rs.next()) {
-				EventDTO dto=new EventDTO();
-				dto.setEvent_no(rs.getInt("event_no"));
-				dto.setEvent_title(rs.getString("event_title"));
-				dto.setEvent_postday(rs.getString("event_postday"));
-				dto.setEvent_view(rs.getString("event_view"));
-				
-				list.add(dto);
-			}
-			return list; //return이 있으면 conn.close()안됨
-		}
 		//=====================content.jsp===============================
 		public EventDTO content(int event_no) throws Exception{
 			
@@ -137,5 +102,93 @@ public class EventDAO {
 			
 			return dto; //return이 있으면 conn.close()안됨
 		}
+			//=====================list.jsp===============================
+			//페이징처리를 위한 객체 전달받기
+			public ArrayList<EventDTO> list() throws Exception{
+				//쿼리생성 - 정렬된 데이터 가져오기
+				String sql="select * from event order by event_no desc";
+				//2 정렬된 데이터에서 순서번호 붙이기
+				sql="select rownum rnum, event_no, event_adm, event_title, event_postday, event_view from ("+sql+")";
+				//3.원하는 부분에 해당되는 데이터 가져오기
+				sql="select event_no, event_adm, event_title, event_postday, event_view from ("+sql+")"
+						+" where rnum between ? and ? ";
+				//심부름꾼
+				PreparedStatement pstmt=conn.prepareStatement(sql);
+				//쿼리실행
+				ResultSet rs = pstmt.executeQuery();
+				
+				//보드디티오를 여러개 담을 수 있는 어레이리스트생성
+				ArrayList<EventDTO> list=new ArrayList<EventDTO>();
+				
+				while(rs.next()) {
+					EventDTO dto=new EventDTO();
+					dto.setEvent_no(rs.getInt("event_no"));
+					dto.setEvent_title(rs.getString("event_title"));
+					dto.setEvent_postday(rs.getString("event_postday"));
+					dto.setEvent_view(rs.getString("event_view"));
+					
+					list.add(dto);
+				}
+				return list; //return이 있으면 conn.close()안됨
+			}
+			public ArrayList<EventDTO> pageList(String pager) throws Exception{
+			   
+			    // limit에 들어갈 index번호 생성
+			    int index;  // limit에 전달할 index값
+//			    int pager;  // 현재 나타내고자 하는 페이지 값을 저장
+			    if(pager==null){
+			    	pager="1";
+			    }
+			    // index 만들기
+			    index=(Integer.parseInt(pager)-1)*10;
+			    // 쿼리 생성
+			     String sql = "select * from (select row_number() over (order by event_no desc) num, A.* from event A order by event_no desc) where num between " 
+			    				+ index + " and " + (index + 10);
+			    // 심부름꾼생성
+			    PreparedStatement pstmt=conn.prepareStatement(sql);
+			    // 쿼리 실행 => ResultSet
+			    ResultSet rs=pstmt.executeQuery();
+			    ArrayList<EventDTO> list=new ArrayList<EventDTO>();
+				
+				while(rs.next()) {
+					EventDTO dto=new EventDTO();
+					dto.setEvent_no(rs.getInt("event_no"));
+					dto.setEvent_title(rs.getString("event_title"));
+					dto.setEvent_postday(rs.getString("event_postday"));
+					dto.setEvent_view(rs.getString("event_view"));
+					
+					list.add(dto);
+				}
+				return list; //return이 있으면 conn.close()안됨
+			}
+			public int getTotalPage() throws Exception {
+			  	 String sql="select count(*) as cnt from event";
+			     PreparedStatement pstmt=conn.prepareStatement(sql);
+		         ResultSet rs=pstmt.executeQuery();
+		         rs.next();
+		         int page_cnt;
+		         page_cnt=rs.getInt("cnt")/10+1;
+		         
+		         if(rs.getInt("cnt")%10 == 0) 
+		             page_cnt--;
+		         return page_cnt;
+			}
+			public int getPstart(int page_cnt, String pager) {
+				 int pstart;
+			       
+		         pstart=Integer.parseInt(pager)/10; 
+		         if(Integer.parseInt(pager)%10 ==0)
+		        	 pstart=pstart-1;
+		         
+		         pstart=Integer.parseInt(pstart+"1");
+		         return pstart;
+			}
+			
+			public int getPend(int page_cnt,int pstart) {
+				int pend=pstart+9;
+				if(page_cnt < pend)
+		        	 pend=page_cnt;
+				return pend;
+			}
 		
 }
