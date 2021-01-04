81b1440bf4692f3d41c839ed16451b3da20b3e53
chj9612 <75474328+chj9612@users.noreply.github.com>
2021-01-04 14:34:21 +0900

    Update LoginDAO.java

--- StampUs2/src/dao/LoginDAO.java
+++ StampUs2/src/dao/LoginDAO.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.catalina.Session;
 
 import dto.LoginDTO;
 
@@ -12,7 +15,7 @@ public class LoginDAO {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
-	 
+	
 	public LoginDAO() {
 		try {
 			String driver = "oracle.jdbc.driver.OracleDriver";
@@ -36,16 +39,53 @@ public class LoginDAO {
 			rs =pstmt.executeQuery();
 			if(rs.next()) {
 				if(rs.getString(1).equals(dto.getPwd())) {
-					return 1; //�α��μ���
+					return 1; //로그인 성공
 				}else {
-					return 0; //��й�ȣ ����ġ
+					return 0; //비밀번호 불일치
 				}
 			}
-			return -1; //���̵� ����
+			return -1; //아이디없음
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		return -2; //�����ͺ��̽�����
+		return -2; //데이터베이스 오류
+	}
+	
+	public String findId(LoginDTO dto) throws Exception {
+		String id = "";
+		String sql = "select USER_ID from USER_MEMBER where EMAIL = ? ";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getEmail());
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				 id = rs.getString("USER_ID");
+			}
+		}catch(Exception e) {
+			 e.printStackTrace();
+		  }finally{
+		   conn.close();
+		  }
+		return id;
+	}
+	public String findPw(LoginDTO dto) throws Exception {
+		String pw = "";
+		String sql = "select USER_PW from USER_MEMBER where USER_ID = ? and USER_NAME = ? and EMAIL = ? ";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getUserid());
+			pstmt.setString(2, dto.getName());
+			pstmt.setString(3, dto.getEmail());
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				pw = rs.getString("USER_PW");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally{
+			conn.close();
+		}
+		return pw;
 	}
 	
 }
